cd1687e5479a19b3c1500fa29154bfee70ceaec7
soda gueye <gueyesoda56@gmail.com>
2023-12-17 18:04:28 +0100

    modification

--- pourBoir.js
+++ pourBoir.js
@@ -1,27 +1,34 @@
 // recuperation id
-let prix = document.getElementById("Bill-input");
-let nbrePers = document.getElementById("Pers-input2");
-let resultat =document.getElementById("Amount");
-let total=document.getElementById("Total");
-let buton1=document.getElementById("btn1")
+// let prix = document.getElementById("Bill-input")
+// let nbrePers = document.getElementById("Pers-input2");
+// let resultat =document.getElementById("Amount");
+// let total=document.getElementById("Total");
+// let buton1=document.getElementById("btn1")
 
-prix.value = "0.0";
-nbrePers.value = "1";
-resultat.innerText = "$ 0.00";
-total.innerText = "$ 0.00";
+
 function button1(){
+    // 100 *15/100 = resultat tip Amount;
+    // 100 / 3 + resulta
+    let prix = document.getElementById("Bill-input")
+    let nbrePers = document.getElementById("Pers-input2")
+    let resultat =document.getElementById("Amount")
+    let total=document.getElementById("Total")
     if(prix.value <=0 || nbrePers.value <=0){
-        alert("saisir un nombre")
+         alert("saisir un nombre")
     }
     else{
         let prix1=(prix.value*15)/(100* nbrePers.value);
         let personne = prix.value/nbrePers.value + prix1;
         resultat.innerText=`$ ${prix1.toFixed(2)}`
         total.innerText=`$ ${personne.toFixed(2)}`
-
-
+        // 
+        // let prix2=(prix.value*5)/(100* nbrePers.value);
+        // let personne2 = prix.value/nbrePers.value + prix1;
+        // resultat.innerText=`$ ${prix2.toFixed(2)}`
+        // total.innerText=`$ ${personne2.toFixed(2)}`
     }
 }
+
 
     
 
